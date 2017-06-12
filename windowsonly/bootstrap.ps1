fe79f2d5d814e98e5aee091f01160064643459a3
Ian <iguy@ionsphere.org>
2017-06-12 14:11:19 -0500

    Add files via upload

---
+++ windowsonly/bootstrap.ps1
@@ -0,0 +1,9 @@
+﻿$profileDir = Split-Path -parent $profile
+
+New-Item $profileDir -itemType Directory -Force -ErrorAction SilentlyContinue
+
+Copy-Item -path ./*.ps1 -Destination $profileDir -exclude "bootstrap.ps1"
+
+Copy-Item -path /home/** -Destination $home -Include **
+
+Remove-Variable profileDir

021fc0b2c884709a7bd0e7bb9a02b522c504a989
Ian K <iguyking@gmail.com>
2020-10-23 10:30:43 -0500

    Clean up git'

--- windowsonly/Microsoft.PowerShell_profile.ps1
+++ windowsonly/Microsoft.PowerShell_profile.ps1
@@ -57,5 +57,6 @@ import-module zlocation
 
 
 function clean-git {
+     git remote prune origin
      git branch -vv | where {$_ -match '\[origin/.*: gone\]'} | foreach {git branch -D ($_.split(" ",[StringSplitOptions]'RemoveEmptyEntries')[0])}
 }
